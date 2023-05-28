45ee1b6e1ac2e560374bdee9e3c47cf2d56b63d6
Philipp Meier <philipp.meier@gmx.at>
2023-05-28 18:58:40 +0200

    Auth. improvements.

--- Program.cs
+++ Program.cs
@@ -1,14 +1,18 @@
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages()
-    .AddRazorPagesOptions(options => {
+    .AddRazorPagesOptions(options =>
+    {
         // To secure all pages.
         // options.Conventions.AuthorizeFolder("/");
-        
+
         // To only secure a specific page.
         options.Conventions.AuthorizePage("/Privacy");
 
@@ -16,35 +20,44 @@ builder.Services.AddRazorPages()
         // options.Conventions.AllowAnonymousToPage("/Privacy");
     });
 
-builder.Services.AddAuthentication(options => {
-        options.DefaultScheme = "cookie";
-        options.DefaultChallengeScheme = "oidc";
-    })
-    .AddCookie("cookie", options => {
-        options.Cookie.Name = builder.Configuration["IdentityProvider:CookieName"];
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(options =>
+{
+    options.Cookie.HttpOnly = true;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    //options.Cookie.SameSite = SameSiteMode.Strict;
 
-        options.Events.OnSigningOut = async e => {
-            await e.HttpContext.RevokeUserRefreshTokenAsync();
-        };
-    })
-    .AddOpenIdConnect("oidc", options => {
-        options.Authority = builder.Configuration["IdentityProvider:Authority"];
-        
-        options.ClientId = builder.Configuration["IdentityProvider:ClientId"];
-        options.ClientSecret = builder.Configuration["IdentityProvider:ClientSecret"];
-        
-        options.Scope.Add("openid");
+    options.Cookie.Name = builder.Configuration["IdentityProvider:CookieName"];
+    options.Events.OnSigningOut = async e => await e.HttpContext.RevokeUserRefreshTokenAsync();
+})
+.AddOpenIdConnect(options =>
+{
+    options.SignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
 
-        options.ResponseType = "code";
-        //options.RequireHttpsMetadata = false;
-        
-        options.TokenValidationParameters = new TokenValidationParameters {
-            ValidateAudience = true,
-            ValidAudience = builder.Configuration["IdentityProvider:ClientId"]
-        };
+    options.Authority = builder.Configuration["IdentityProvider:Authority"];
+    options.ClientId = builder.Configuration["IdentityProvider:ClientId"];
+    options.ClientSecret = builder.Configuration["IdentityProvider:ClientSecret"];
+    options.ResponseType = OpenIdConnectResponseType.Code;
+    options.ResponseMode = OpenIdConnectResponseMode.Query;
+    options.RequireHttpsMetadata = true;
+    //options.GetClaimsFromUserInfoEndpoint = true;
 
-        options.SaveTokens = true;
-    });
+    options.Scope.Add("openid");
+
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = options.Authority,
+        ValidAudience = options.ClientId,
+        ValidateIssuer = true,
+        ValidateAudience = true,
+    };
+
+    options.SaveTokens = true;
+});
 
 var app = builder.Build();
 
